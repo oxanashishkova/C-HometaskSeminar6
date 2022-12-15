cb06707183ed42c6e0886bb72fb1550c2018f21f
«Oksana <oxana.shishkova@gmail.com>
2022-12-15 17:56:38 +0300

    Start committing hometask Seminar 6

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+//0, 7, 8, -2, -2 -> 2
+//1, -7, 567, 89, 223-> 3
+
+Console.WriteLine("Введите элементы массива, разделенные пробелом:");
+string massive= Console.ReadLine();
+
+var arraystr = massive.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+int[] array = Array.ConvertAll(arraystr, Convert.ToInt32);
+int sum = 0;
+
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i]>0)
+    {
+        sum++;
+    }
+}
+Console.WriteLine($"Количество элементов больше нуля: {sum}");
